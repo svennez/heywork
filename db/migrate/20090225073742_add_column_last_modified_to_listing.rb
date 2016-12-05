7bcf37c04eb67de18188c5369821426b8d9ea242
anjain <ror.upwork@gmail.com>
2016-12-05 20:36:51 +0530

    query changes in migrations

--- db/migrate/20090225073742_add_column_last_modified_to_listing.rb
+++ db/migrate/20090225073742_add_column_last_modified_to_listing.rb
@@ -1,7 +1,7 @@
 class AddColumnLastModifiedToListing < ActiveRecord::Migration
   def self.up
     add_column :listings, :last_modified, :datetime
-    Listing.find(:all).each { |listing| listing.update_attribute :last_modified, listing.created_at}
+    Listing.all.each { |listing| listing.update_attribute :last_modified, listing.created_at}
   end
 
   def self.down
--- db/migrate/20090330072036_add_settings_to_existing_people.rb
+++ db/migrate/20090330072036_add_settings_to_existing_people.rb
@@ -1,12 +1,12 @@
 class AddSettingsToExistingPeople < ActiveRecord::Migration
   def self.up
-    Person.find(:all).each do |person| 
+    Person.all.each do |person| 
       person.settings = Settings.create
     end  
   end
 
   def self.down
-    Person.find(:all).each do |person| 
+    Person.all.each do |person| 
       person.settings = nil
     end
   end
