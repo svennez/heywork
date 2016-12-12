430bb8a53c2c1baa8fc3593aad7449fca96d56b1
anjain <ror.upwork@gmail.com>
2016-12-12 13:57:20 +0530

    map issue resolved

--- app/view_utils/marketplace_helper.rb
+++ app/view_utils/marketplace_helper.rb
@@ -3,7 +3,7 @@ module MarketplaceHelper
 
   def google_maps_key(cid)
     community_key = Community.where(id: cid).pluck(:google_maps_key).first
-    community_key ? community_key : APP_CONFIG.google_maps_key
+    community_key ? community_key : ENV['google_maps_key']
   end
 
   def style_customizations_map(community)
